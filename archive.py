910181cb1d070a95abcc8e7db1a59c41bc382c9a
Arkiver2 <Arkiver@hotmail.com>
2015-04-29 11:25:51 +0200

    archive.py: fix decoding issues

--- archive.py
+++ archive.py
@@ -18,6 +18,7 @@ import time,json
 import os
 import datetime
 import shutil
+import codecs
 
 # Create the inbox.py object
 inbox = Inbox()
@@ -26,6 +27,7 @@ attachmentsjson = []
 command = False
 newindex = False
 newmails = False
+updatescripts = False
 
 @inbox.collate
 # Our message handling function
@@ -33,6 +35,7 @@ def handle(rawdata, to, sender, subject, mailhtml, mailplain, attachments, tonam
 	global newindex
 	global command
 	global newmails
+	global updatescripts
 	if sender == "arkiver@hotmail.com" or sender == "chpwssn@gmail.com":
 		if subject == "create new index.html":
 			newindex = True
@@ -68,16 +71,16 @@ def handle(rawdata, to, sender, subject, mailhtml, mailplain, attachments, tonam
 		directoryName = timereceived
 		humantime = datetime.datetime.fromtimestamp(int(timereceived)).strftime('%Y-%m-%d %H:%M:%S')
 		if not os.path.exists(baseDirectory+"index.html"):
-			with open(baseDirectory+"index.html", "w") as mainindex:
+			with codecs.open(baseDirectory+"index.html", "w", encoding="utf-8") as mainindex:
 				mainindex.write('<!--TABLE VERSION-->')
 				mainindex.write("<html><head><title>Newsletter Archive (WIP)</title></head><body>")
 				mainindex.write('<style type="text/css">.table {border-collapse:collapse;border-spacing:0}.table td{padding:10px 5px;border-style:solid;border-width:0px;word-break:normal;background-color:#E6E6E6;border-top-width:1px;border-bottom-width:1px;}.table th{padding:10px 5px;border-style:solid;border-width:0px;word-break:normal;background-color:#BDBDBD;border-top-width:1px;border-bottom-width:1px;}.table .table-b{background-color:C1C1C1}')
 				mainindex.write('</style><table class="table"><tr><th class="table-a">DATE</th><th class="table-a">FROM</th><th class="table-a">SUBJECT</th><th class="table-a">LENGTH</th>')
 				mainindex.write('</tr></table>')
 				mainindex.write("</body></html>")
-		open(baseDirectory+"index.html-temp", "w").close()
+		codecs.open(baseDirectory+"index.html-temp", "w", encoding="utf-8").close()
 		basedir = open(baseDirectory+"index.html")
-		basedirtemp = open(baseDirectory+"index.html-temp", "a")
+		basedirtemp = codecs.open(baseDirectory+"index.html-temp", "a", encoding="utf-8")
 		for text in basedir:
 			if '<th class="table-a">DATE</th><th class="table-a">FROM</th><th class="table-a">SUBJECT</th><th class="table-a">LENGTH</th></tr><tr><td class="table-b">' in text:
 				text = text.replace('<th class="table-a">DATE</th><th class="table-a">FROM</th><th class="table-a">SUBJECT</th><th class="table-a">LENGTH</th>','<th class="table-a">DATE</th><th class="table-a">FROM</th><th class="table-a">SUBJECT</th><th class="table-a">LENGTH</th></tr><tr><td class="table-a">'+humantime+'</td><td class="table-a">'+sender+'</td><td class="table-a"><a href="'+directoryName+'">'+subject+'</a></td><td class="table-a">'+str(len(rawdata))+'</td>')
@@ -92,13 +95,13 @@ def handle(rawdata, to, sender, subject, mailhtml, mailplain, attachments, tonam
 		#Check to see if the directory we are going to write to exists
 		if not os.path.isdir(baseDirectory+directoryName):
 			os.makedirs(baseDirectory+directoryName)
-		with open(baseDirectory+directoryName+"/index.html","w") as messageindex:
+		with codecs.open(baseDirectory+directoryName+"/index.html", "w", encoding="utf-8") as messageindex:
 			messageindex.write("<html><head><title>"+directoryName+"</title></head><body>ALL DATA:&nbsp;<a href='"+directoryName+".json"+"'>JSON</a><br/>")
 			messageindex.write("ATTACHMENT(S):&nbsp;")
 			for attachment in attachments:
 				if not os.path.isdir(baseDirectory+directoryName+"/attachments"):
 					os.makedirs(baseDirectory+directoryName+"/attachments")
-				with open(baseDirectory+directoryName+"/attachments/"+attachment[2],"w") as file:
+				with open(baseDirectory+directoryName+"/attachments/"+attachment[2], "w") as file:
 					file.write(attachment[1])
 				messageindex.write("<a href='attachments/"+attachment[2]+"'>"+attachment[2]+"</a>&nbsp;")
 				print "Wrote attachment "+attachment[2]
@@ -109,13 +112,13 @@ def handle(rawdata, to, sender, subject, mailhtml, mailplain, attachments, tonam
 			messageindex.write("SUBJECT:&nbsp;"+subject+"<br/>")
 			messageindex.write("DATE:&nbsp;"+humantime+"<br/>")
 			# Write the components to the .json file, better for processing later but doesn't solve encoding
-			with open(baseDirectory+directoryName+"/"+directoryName+".json","w") as jsonfile:
-				jsonfile.write(json.dumps({"rawdata":rawdata, "to":to, "sender":sender, "subject":subject, "mailhtml":mailhtml, "mailplain":mailplain, "attachments":attachmentsjson, "toname":toname, "sendername":sendername}))
+			with codecs.open(baseDirectory+directoryName+"/"+directoryName+".json", "w", encoding="utf-8") as jsonfile:
+				jsonfile.write(json.dumps({"rawdata":rawdata, "to":to, "sender":sender, "subject":subject, "mailhtml":mailhtml, "mailplain":mailplain, "attachments":attachmentsjson, "toname":toname, "sendername":sendername}, ensure_ascii=False))
 			# Write the html body to a html file by itself
-			with open(baseDirectory+directoryName+"/"+directoryName+"-mailhtml.html","w") as mailhtmlfile:
+			with codecs.open(baseDirectory+directoryName+"/"+directoryName+"-mailhtml.html", "w", encoding="utf-8") as mailhtmlfile:
 				mailhtmlfile.write(mailhtml)
 				messageindex.write("<iframe style='width:100%;height:45%'  src='"+directoryName+"-mailhtml.html'></iframe><br/>")
-			with open(baseDirectory+directoryName+"/"+directoryName+"-mailplain.txt","w") as mailplainfile:
+			with codecs.open(baseDirectory+directoryName+"/"+directoryName+"-mailplain.txt", "w", encoding="utf-8") as mailplainfile:
 				mailplainfile.write(mailplain)
 				messageindex.write("<iframe style='width:100%;height:45%'  src='"+directoryName+"-mailplain.txt'></iframe><br/>")
 			print "Wrote "+directoryName+".json"
