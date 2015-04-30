aca17776e2a25205d8e90273bf20730b3ef6bccc
Arkiver2 <Arkiver@hotmail.com>
2015-04-30 12:50:42 +0200

    update.sh

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#An auto update tool
+CURLOG=$(cat curlog.txt)
+CURLOG=$(($CURLOG + 1))
+mv log.txt log.txt.$CURLOG
+echo $CURLOG > curlog.txt
+touch log.txt
+chmod 777 log.txt
+echo "Log rotated to .$CURLOG"
+git pull
